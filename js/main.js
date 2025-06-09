9a9be6cbaed6adb33de0def303852bcd925c493b
[MMTsonkov23] <[MMTsonkov23@codingburgas.bg]>
2025-06-09 19:34:52 +0300

    Add a log in page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.getElementById('navbar');
+  if (!navbar) return;
 
+  const links = navbar.getElementsByTagName('a');
+  const currentPage = window.location.pathname.split("/").pop();
+
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].getAttribute('href') === currentPage) {
+      links[i].classList.add('active');
+    }
+  }
+});
